420eaddb7a2ed71731513b55e487eaed4d304a4c
WLD <alex.p@wearabledevices.co.il>
2026-08-07 05:16:43 +0700

    chore(p5): remove ungranted source inventory delta

--- test/product/source_contract_test.dart
+++ test/product/source_contract_test.dart
@@ -126,15 +126,6 @@ void main() {
     test('only the governed library source is analyzer-visible', () {
       const expected = <String>{
         'lib/main.dart',
-        'lib/p5_ia_preview.dart',
-        'lib/product/p5_information_architecture/p5_components.dart',
-        'lib/product/p5_information_architecture/p5_controller.dart',
-        'lib/product/p5_information_architecture/p5_fixtures.dart',
-        'lib/product/p5_information_architecture/p5_models.dart',
-        'lib/product/p5_information_architecture/p5_prototype.dart',
-        'lib/product/p5_information_architecture/p5_support_workspaces.dart',
-        'lib/product/p5_information_architecture/p5_task_workspaces.dart',
-        'lib/product/p5_information_architecture/p5_verification_workspaces.dart',
         'lib/product/agent_decision.dart',
         'lib/product/agent_protocol.dart',
         'lib/product/api_server.dart',
