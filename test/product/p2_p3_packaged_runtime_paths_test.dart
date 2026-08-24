8858538cbf4adfa6022ef47523f74514c63da127
WLD <alex.p@wearabledevices.co.il>
2026-08-24 21:12:13 +0300

    chore(ci): remove manifest sync trigger

--- test/product/p2_p3_packaged_runtime_paths_test.dart
+++ test/product/p2_p3_packaged_runtime_paths_test.dart
@@ -4,7 +4,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:kristin_local_agent/product/browser/browser_runtime_bundle.dart';
 import 'package:kristin_local_agent/product/p2_runtime_resource_resolver.dart';
 
-// Manifest synchronization trigger; no behavioral change.
 String _nativePath(Directory root, List<String> parts) =>
     <String>[root.absolute.path, ...parts].join(Platform.pathSeparator);
 
