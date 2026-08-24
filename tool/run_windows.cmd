c42701c30fad3036b51afbf301ce43ec672851ad
WLD <alex.p@wearabledevices.co.il>
2026-08-24 13:26:21 +0300

    dev: remove unconditional Windows clean and full verify

--- tool/run_windows.cmd
+++ tool/run_windows.cmd
@@ -11,14 +11,13 @@ if errorlevel 1 (
 call "%~dp0prune_stale_legacy.cmd"
 if errorlevel 1 exit /b 1
 
-call flutter clean
-if errorlevel 1 exit /b 1
-
 call "%~dp0bootstrap_platforms.cmd"
 if errorlevel 1 exit /b 1
 
-call "%~dp0verify.cmd"
-if errorlevel 1 exit /b 1
+if /I "%KRISTIN_VERIFY_BEFORE_RUN%"=="1" (
+  call "%~dp0verify.cmd"
+  if errorlevel 1 exit /b 1
+)
 
 call flutter run -d windows
 if errorlevel 1 exit /b 1
